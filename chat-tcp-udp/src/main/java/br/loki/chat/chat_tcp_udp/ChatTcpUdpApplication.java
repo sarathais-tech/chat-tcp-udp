11b967d0e7ba06ad8b04967f04ac7d797d06c2f8
sasacatt <159670375+sasacatt@users.noreply.github.com>
2026-03-03 11:56:23 -0300

    Initial commit - Spring Boot TCP UDP project

---
+++ chat-tcp-udp/src/main/java/br/loki/chat/chat_tcp_udp/ChatTcpUdpApplication.java
@@ -0,0 +1,13 @@
+package br.loki.chat.chat_tcp_udp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatTcpUdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatTcpUdpApplication.class, args);
+	}
+
+}
---
+++ chat-tcp-udp/src/test/java/br/loki/chat/chat_tcp_udp/ChatTcpUdpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.loki.chat.chat_tcp_udp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatTcpUdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
